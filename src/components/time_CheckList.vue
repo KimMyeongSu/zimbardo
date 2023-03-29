4af8a0e0c39490a40baa2c0dae34df70093d0f12
KimMyeongSu <Myeongsu@likelion.org>
2023-03-29 13:36:43 +0000

    리스트 출력

--- src/components/time_CheckList.vue
+++ src/components/time_CheckList.vue
@@ -1,12 +1,15 @@
 <template>
   <div>
     <h1>Component Check List</h1>
-    <h1>{{ listData }}</h1>
+    <!-- <h1>{{ listData }}</h1> -->
+    <h3 v-for="item in listData" v-bind:key="item.index">
+      {{ item.idx + ".  " + item.text }}
+    </h3>
   </div>
 </template>
 
 <script lang="js">
-import { data } from '../store/data.json'
+import { data } from '../store/test.json'
 export default {
     name: 'CheckList',
     data() {
